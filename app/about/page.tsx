aa94ea37ca11059a1a8ce25e9114be09799b0aad
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-09-05 21:46:08 +0300

    Revert unwanted changes in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
         <div className="text-center max-w-3xl">
           <h1 className="text-4xl font-bold mb-6 text-zinc-300">About</h1>
           <p className="text-sm text-zinc-400 leading-relaxed">
-            Driven by a passion for both creativity and technology, I build intuitive digital experiences and continuously challenge the limits of software development. I strive to create innovative solutions that blend form and function, pushing the boundaries of what's possible in the digital realm. Bir sana yandım ben, bir sana kitapsız. Bir sana aldandım bir sana vefasız. Aahahahahahaha eylül geldi hoş geli, sıcaklar bitti güzel bir sonbahar bizi bekliyor. 
+            Driven by a passion for both creativity and technology, I build intuitive digital experiences and continuously challenge the limits of software development. I strive to create innovative solutions that blend form and function, pushing the boundaries of what's possible in the digital realm.
           </p>
         </div>
       </div>
