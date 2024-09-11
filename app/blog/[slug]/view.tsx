a48ebc83089d53dc7c66b18ac4d31b7b754746a8
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-09-11 16:55:41 +0300

    update and generate blog pages

--- app/blog/[slug]/view.tsx
+++ app/blog/[slug]/view.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export const ReportView: React.FC<{ slug: string }> = ({ slug }) => {
-	useEffect(() => {
-		fetch("/api/incr", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ slug, type: "blog" }),
-		});
-	}, [slug]);
+  const reported = useRef(false);
 
-	return null;
+  useEffect(() => {
+    if (reported.current) return;
+
+    const registerView = () =>
+      fetch("/api/incr", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ slug, type: "blog" }),
+      });
+
+    registerView();
+    reported.current = true;
+  }, [slug]);
+
+  return null;
 };
