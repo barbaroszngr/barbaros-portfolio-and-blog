1e00726ee442141e62b480ae82ae85e75d4fb2ce
Barbaros ZÖNGÜR <140146415+barbaroszngr@users.noreply.github.com>
2024-09-19 16:18:44 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,20 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
-	images: {
-		domains: ["miro.medium.com"],
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
+  images: {
+    domains: ['miro.medium.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+        pathname: '/v2/**',
+      },
+    ],
+  },
 };
 
 export default withContentlayer(nextConfig);
-
